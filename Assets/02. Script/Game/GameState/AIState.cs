1793ff6cd5f44342f9ce9562cf56a667081c01ac
ZeroDimen <rlaehdrjs951@naver.com>
2025-09-16 18:09:41 +0900

    오목 AI 알고리즘 연산 개선
    착수한 지점 주변을 후보군으로 정하는 것으로 연산량 감소
    각 후보군에서 호출하는 DoMiniMax 함수를 비동기로 연산하여 연산속도 개선
    착수한 지점에서만의 승리 판정 함수 연산으로 연산 속도 개선
    다양한 형태의 평가 함수 추가로 AI 알고리즘의 실력 개선

--- Assets/02. Script/Game/GameState/AIState.cs
+++ Assets/02. Script/Game/GameState/AIState.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
 using UnityEngine;
-using System.Threading.Tasks; // Task를 사용하기 위해 필요
 
 /// <summary>
 /// 작성자 : 김동건
@@ -10,21 +9,21 @@ public class AIState : BasePlayerState {
     /// <summary>
     /// 작성자 : 김동건
     /// AI의 차례가 되면 현재 보드를 가져와 최적의 수를 연산하는 함수 GetBestMove를 호출함
-    /// GetBestMove의 연산 시간이 너무 오래 걸리므로 화면이 멈춘 것처럼 보임 따라서 비동기 연산을 함으로써
-    /// 설정 창과 상호작용은 물론 게임이 진행되는 것처럼 보임
+    /// GetBestMove의 후보군마다 Minimax 알고리즘 연산 시간이 너무 오래 걸리므로 화면이 멈춘 것처럼 보임
+    /// 따라서 GetBestMove의 후보군의 DoMiniMax 함수를 비동기 연산을 함으로써 속도 개선 및 연산 중 상호작용 가능
     /// </summary>
     /// <param name="gameLogic"></param>
-    public override async void OnEnter(GameLogic gameLogic) { 
-        
+    public override async void OnEnter(GameLogic gameLogic) {
+        // 턴 표시
         GameManager.Instance.SetGameTurnPanel(GameUIController.GameTurnPanelType.BTurn);
-
+        
+        // AI 처리
         var board = gameLogic.GetBoard();
-
-        // GetBestMove 연산을 백그라운드 스레드에서 실행
-        var result = await Task.Run(() => OmokAI.GetBestMove(board)); 
-        // await 덕분에 이 다음 라인은 GetBestMove가 완료될 때까지 기다립니다.
-
-        // GetBestMove 연산 완료 후 다시 메인 스레드로 돌아와 처리
+        
+        // GetBestMove 연산을 백그라운드 스레드에서 실행, await 때문에 GetBestMove 함수가 완료 될 때까지 대기
+        (int row, int col)? result = await OmokAI.GetBestMove(board);
+        
+        
         if (result.HasValue) {
             HandleMove(gameLogic, result.Value.row, result.Value.col);
         }
@@ -33,14 +32,14 @@ public class AIState : BasePlayerState {
         }
     }
 
-    public override void HandleMove(GameLogic gameLogic, int row, int col) {
-        ProcessMove(gameLogic, Constants.PlayerType.PlayerB, row, col);
-    }
-
     protected override void HandleNextTurn(GameLogic gameLogic) {
         gameLogic.SetState(gameLogic.firstPlayerState);
     }
 
+    public override void HandleMove(GameLogic gameLogic, int row, int col) {
+        ProcessMove(gameLogic, Constants.PlayerType.PlayerB, row, col);
+    }
+    
     public override void OnExit(GameLogic gameLogic) {
     }
 }
--- Assets/02. Script/Game/GameState/BasePlayerState.cs
+++ Assets/02. Script/Game/GameState/BasePlayerState.cs
@@ -14,12 +14,19 @@ public abstract class BasePlayerState
     public abstract void OnExit(GameLogic gameLogic);       // 상태가 종료
     public abstract void HandleMove(GameLogic gameLogic, int row, int col);     // 마커 표시
     protected abstract void HandleNextTurn(GameLogic gameLogic);    // 턴 전환
-
-    // 게임 결과 처리
+    
+    /// <summary>
+    /// 작성자 : 김동건
+    /// 좌표를 입력받아 착수 후 게임 결과 처리
+    /// </summary>
+    /// <param name="gameLogic"></param>
+    /// <param name="playerType"></param>
+    /// <param name="row"></param>
+    /// <param name="col"></param>
     protected void ProcessMove(GameLogic gameLogic, Constants.PlayerType playerType, int row, int col) {
         if(gameLogic.SetNewBoardValue(playerType, row, col)) {
-            // 새롭게 놓여진 Markwer를 기반으로 게임의 결과를 판단
-            gameResult = gameLogic.CheckGameResult();
+            // 새롭게 놓여진 Marker를 기반으로 게임의 결과를 판단
+            gameResult = gameLogic.CheckGameResult(row , col);
 
             if(gameResult == GameLogic.GameResult.None) {
                 HandleNextTurn(gameLogic);
